befa24c67f9254064ef5999e4dd004d24105b5a0
Ajay <ajay.singh.114085@gmail.com>
2024-07-25 23:36:49 +0530

    Method overriding

---
+++ MethodOverriding.java
@@ -0,0 +1,23 @@
+class A {
+    void display() {
+        System.out.println("Parent class.");
+    }
+}
+
+class Child extends A {
+    void display() {
+        System.out.println("Child class.");
+    }
+
+    void show() {
+        // Call the display method of the Parent class
+        super.display();
+    }
+}
+
+public class MethodOverriding {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.show();  // This will call the display method of the Parent class
+    }
+}
