d62788e96b60e455f6503fe37caa369221e5551d
Ajay <ajay.singh.114085@gmail.com>
2025-04-13 13:23:08 +0530

    Threads through Lambda : -> example 2

--- lambda/threads/ThreadDemo.java
+++ lambda/threads/ThreadDemo.java
@@ -20,7 +20,26 @@ public class ThreadDemo {
         t1.setName("Jhon-Wick");
         Thread t2 = new Thread(thread1);
         t2.setName("Captan America");
-        t2.start();
-        t1.start();
+       // t2.start();
+        //t1.start();
+
+
+        // example 2 - Runnable
+
+        Runnable t22 = ()->{
+            try {
+                System.out.println(Thread.currentThread().getName());
+                for(int i=1;i<=10;i++){
+                    System.out.println(Thread.currentThread().getName()+" :"+i*2);
+                    Thread.sleep(100);
+                }
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+
+        };
+        Thread thread22 = new Thread(t22);
+        thread22.setName("Table");
+        thread22.start();
     }
 }
